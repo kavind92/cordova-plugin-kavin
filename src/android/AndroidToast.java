033deeab7aa300987b2d84116a8f53a2efaecd0d
Kavin <kavind92@gmail.com>
2019-11-13 12:52:30 +0530

    v1.0.1 release

--- src/android/AndroidToast.java
+++ src/android/AndroidToast.java
@@ -1,30 +1,30 @@
-package com.nikolabreznjak;
+package com.letsdosimple.cordova.plugin;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.json.JSONException;
-import android.content.Context;
-import android.widget.Toast;
 
-public class AndroidToast extends CordovaPlugin {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+public class AndroidKavin extends CordovaPlugin {
+
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if ("show".equals(action)) {
-            show(args.getString(0), callbackContext);
+        if (action.equals("show")) {
+            String message = args.getString(0);
+            this.show(message, callbackContext);
             return true;
         }
-
         return false;
     }
-
-    private void show(String msg, CallbackContext callbackContext) {
-        if (msg == null || msg.length() == 0) {
-            callbackContext.error("Empty message!");
+    
+    private void show(String message, CallbackContext callbackContext) {
+        if (message != null && message.length() > 0) {
+            callbackContext.success(message);
         } else {
-            Toast.makeText(webView.getContext(), msg, Toast.LENGTH_LONG).show();
-            callbackContext.success(msg);
+            callbackContext.error("Expected one non-empty string argument - error in java file.");
         }
     }
-}
+    }
